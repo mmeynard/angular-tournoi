74bd6f965011e2ff53c3adccff8f53637fe64c84
mmeynard <michel.meynard@free.fr>
2019-10-15 10:38:56 +0200

    1 file modified, 3 added, 4 deleted

---
+++ src/app/tournoi.ts
@@ -0,0 +1,24 @@
+export const tournois = [
+  {
+    name: 'Master 2019',
+    date: '2019/07/14',
+    description: 'Pour les licenciés niveau Prénat minimum'
+  },
+  {
+    name: 'M13 2019',
+    date: '2019/07/14',
+    description: 'Pour les moins de 13 ans'
+  },
+  {
+    name: 'Féminin 2019',
+    date: '2019/07/14',
+    description: 'Tournoi réservé aux équipes féminines'  
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/liste-tournoi/liste-tournoi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-tournoi',
+  templateUrl: './liste-tournoi.component.html',
+  styleUrls: ['./liste-tournoi.component.css']
+})
+export class ListeTournoiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
